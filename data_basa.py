a0a1fe66b0133433da748742efe4b21e2fbd0ed5
zakii1123 <127938088+zakii1123@users.noreply.github.com>
2023-05-06 18:18:39 +0200

    Update data_basa.py

--- data_basa.py
+++ data_basa.py
@@ -11,8 +11,22 @@ data = tuple(data) #Все элементы из бд
 
 lst = list(data) #Переведение в лист(хз зачем, но подумал так надо)
 
-for item in lst:
-    print(item, end ='\n')#Разделение всех элементов
+# for item in lst:
+#     print(item, end ='\n')#Разделение всех элементов
 
+def recomend():
+    item = list(choice(lst))
+    tag = item[0]
+    name = item[1]
+    janr = item[2]
+    year_of_create = item[3]
+    creator = item[4]
+    print(f'Рекомендация игры: {name}. Жанр: {janr}. Год выпуска: {year_of_create}. Создатели: {creator}.')
 
-
+recomend()    
+answer = input('Хотите изменить игру ? y/n: ')
+answer = answer.lower()
+if answer == 'y':
+    recomend()
+else: 
+    print("Ok")
